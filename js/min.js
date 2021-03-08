3c84f44289c08211ed21e8b105da21f858839e64
RobinsonF <rgutierrezs@unbosque.edu.co>
2021-03-08 18:48:00 -0500

    Tabla de animales, falta paginación

--- js/min.js
+++ js/min.js
@@ -8,6 +8,38 @@ d3.dsv(";","../Data/PetsCitizens.csv")
 .catch(function(error){
   //handle error
 });
+
+
 function printData(){
-  console.log(allData);
+  table = document.getElementById("table");
+  for(var i = 1; i < table.rows.length; i++)
+        {
+          for(var j = 0; j < table.rows[i].cells.length; j++)
+          { 
+              table.rows[i].cells[0].innerHTML = allData[i-1][j];
+          }
+          for(var j = 0; j < table.rows[i].cells.length; j++)
+          { 
+              table.rows[i].cells[1].innerHTML = allData[i-1]["species"];
+          }
+          for(var j = 0; j < table.rows[i].cells.length; j++)
+          { 
+              table.rows[i].cells[2].innerHTML = allData[i-1]["sex"];
+          }
+          for(var j = 0; j < table.rows[i].cells.length; j++)
+          { 
+              table.rows[i].cells[3].innerHTML = allData[i-1]["size"];
+          }
+          for(var j = 0; j < table.rows[i].cells.length; j++)
+          { 
+              table.rows[i].cells[4].innerHTML = allData[i-1]["potentDangerous"];
+          }
+          for(var j = 0; j < table.rows[i].cells.length; j++)
+          { 
+              table.rows[i].cells[5].innerHTML = allData[i-1]["neighborhood"];
+          }
+}
+console.log(allData[0].sex);
+allData[0] = {...allData[0],sex : "MACHO"};
+console.log(allData[0].sex);
 }
